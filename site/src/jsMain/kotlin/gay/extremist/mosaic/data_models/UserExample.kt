ea93cfb1f1532378eb95ef07f91c8e1ee687c088
Fox Johnjulio <foxkj01@gmail.com>
2024-03-29 00:58:39 -0400

    Readd dataclass

---
+++ site/src/jsMain/kotlin/gay/extremist/mosaic/data_models/UserExample.kt
@@ -0,0 +1,37 @@
+package gay.extremist.mosaic.data_models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Geo(
+    val lat: String,
+    val lng: String
+)
+
+@Serializable
+data class Address(
+    val street: String,
+    val suite: String,
+    val city: String,
+    val zipcode: String,
+    val geo: Geo
+)
+
+@Serializable
+data class Company(
+    val name: String,
+    val catchPhrase: String,
+    val bs: String
+)
+
+@Serializable
+data class User(
+    val id: Int,
+    val name: String,
+    val username: String,
+    val email: String,
+    val address: Address,
+    val phone: String,
+    val website: String,
+    val company: Company
+)
