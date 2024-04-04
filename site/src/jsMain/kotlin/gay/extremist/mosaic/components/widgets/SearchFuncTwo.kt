c9ebf66798f9b9f78b28679ed147e9c97bc3f8d6
Fox Johnjulio <foxkj01@gmail.com>
2024-04-04 12:29:42 -0400

    feat: fix searchbar

--- site/src/jsMain/kotlin/gay/extremist/mosaic/components/widgets/SearchFuncTwo.kt
+++ site/src/jsMain/kotlin/gay/extremist/mosaic/components/widgets/SearchFuncTwo.kt
@@ -1,6 +1,27 @@
 package gay.extremist.mosaic.components.widgets
 
-/**
+import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.css.TextAlign
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.attrsModifier
+import com.varabyte.kobweb.compose.ui.graphics.Color
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.toAttrs
+import com.varabyte.kobweb.silk.components.forms.*
+import com.varabyte.kobweb.silk.components.icons.fa.FaMagnifyingGlass
+import com.varabyte.kobweb.silk.components.style.addVariantBase
+import com.varabyte.kobweb.silk.components.style.vars.color.ColorVar
+import com.varabyte.kobweb.silk.theme.colors.palette.background
+import com.varabyte.kobweb.silk.theme.colors.palette.toPalette
+import io.ktor.client.*
+import org.jetbrains.compose.web.attributes.ButtonType
+import org.jetbrains.compose.web.attributes.onSubmit
+import org.jetbrains.compose.web.css.cssRem
+import org.jetbrains.compose.web.dom.Form
+import org.jetbrains.compose.web.dom.Text
+
+
 val SearchBarInput by InputGroupStyle.addVariantBase {
     Modifier
         .setVariable(InputVars.BorderColor, Colors.Transparent)
@@ -9,50 +30,48 @@ val SearchBarInput by InputGroupStyle.addVariantBase {
 }
 
 @Composable
-fun SearchForm(viewModel: DataPageVM.SearchBarVM) {
+fun SearchForm(httpClient: HttpClient) {
     Form(
-        attrs = DarkBackgroundBoxStyle.toModifier()
+        attrs = Modifier
             .fillMaxWidth()
             .margin(topBottom = 0.5.cssRem)
             .textAlign(TextAlign.Center)
             .toAttrs {
                 onSubmit {
                     it.preventDefault() // This stops the form from "submitting"
-                    viewModel.onEnterSearch()
+                    // TODO Perform search
                 }
             }
     ) {
         val dataListId = "search-list"
-        key(viewModel.suggestions) {
-            Datalist(Modifier.id(dataListId).toAttrs()) {
-                viewModel.suggestions.forEach {
-                    Option(it) // tried using key() but it didn't seem to help
-                }
-            }
-        }
-        SearchBar(dataListId, viewModel)
+//        key(/**TODO Create suggestions list**/) {
+//            Datalist(Modifier.id(dataListId).toAttrs()) {
+//                viewModel.suggestions.forEach {
+//                    Option(it) // tried using key() but it didn't seem to help
+//                }
+//            }
+//        }
+        SearchBar(dataListId, httpClient)
         Text("Search and select an instructor or subject, or enter a course code")
     }
 }
 
 @Composable
-private fun SearchBar(dataListId: String, viewModel: DataPageVM.SearchBarVM) {
+private fun SearchBar(dataListId: String, httpClient: HttpClient) {
+    var textInput by remember { mutableStateOf("") }
     InputGroup(Modifier.margin(bottom = 0.25.cssRem), variant = SearchBarInput) {
         TextInput(
-            viewModel.input,
-            { viewModel.input = viewModel.inputTransform(it) },
-            Modifier
-                .onClick { viewModel.active = true }
-                .attrsModifier { attr("list", dataListId) },
-            placeholder = viewModel.placeholder,
+            textInput,
+            { textInput = it },
+            Modifier.onClick {  }.attrsModifier { attr("list", dataListId) },
+            placeholder = "",
             focusBorderColor = Colors.Transparent,
         )
         RightInset {
             Button(
                 onClick = {},
                 modifier = Modifier.ariaLabel("Search"),
-                variant = UnstyledButtonVariant,
-                size = UnsetButtonSize,
+                variant = SearchBarInput,
                 type = ButtonType.Submit,
             ) {
                 FaMagnifyingGlass()
@@ -60,4 +79,3 @@ private fun SearchBar(dataListId: String, viewModel: DataPageVM.SearchBarVM) {
         }
     }
 }
-**/
